647faabe9c3319f412f196ee259ddd5ae090aa7f
karina <karina.kulakova.97@gmail.com>
2023-10-31 15:31:35 +0200

    Add the database exception filter

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -22,6 +22,8 @@ namespace ContosoUniversity
             services.AddDbContext<SchoolContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
             services.AddControllersWithViews();
         }
     }
